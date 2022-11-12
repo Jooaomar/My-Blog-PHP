8cff06d20b30ae4f3064ab3dc9322e67d4ee1b72
Jooaomar <jmrcl91@gmail.com>
2022-11-12 15:39:09 -0300

    first commit

---
+++ templates/top-menu.php
@@ -0,0 +1,19 @@
+<div class="top-menu">
+    <div class="menu-options">
+        <?php if (isLoggedIn()): ?>
+            <a href="index.php">Home</a>
+            |
+            <a href="list-posts.php">All posts</a>
+            |
+            <a href="edit-post.php">New post</a>
+            |
+            Hello <?php echo htmlEscape(getAuthUser()) ?>.
+            <a href="logout.php">Log out</a>
+        <?php else: ?>
+            <a href="login.php">Log in</a>
+        <?php endif ?>
+    </div>
+    <a href="index.php">
+        <h1>Blog title</h1>
+    </a>
+</div>
